a27584a282b37c039da730fe59b57d36e799e80f
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-09-28 13:32:45 +0200

    Update app.config.ts: Add provideHttpClient withFetch() to providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,15 @@ import {
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch()),
   ],
 };
